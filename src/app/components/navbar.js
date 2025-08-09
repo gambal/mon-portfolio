a9b4ff20cb088b1b6f77029a158442ca6d9f9d4c
gambal <agambarini.info@gmail.com>
2025-08-09 19:59:43 +0200

    Je me lance par la fenetre si c'est ça

--- src/app/components/navbar.js
+++ src/app/components/navbar.js
@@ -1,21 +1,21 @@
-// // src/app/components/navbar.js
-// import Link from 'next/link';
-// import './navbar.css'; // Import du fichier CSS
+// src/app/components/navbar.js
+import Link from 'next/link';
+import './navbar.css'; // Import du fichier CSS
 
-// export default function Navbar() {
-//   return (
-//     <nav className="navbar">
-//       <div className="navbar__mobile-name">
-//         <span><Link href="/home">alexandre</Link></span>
-//         <span><Link href="/home">gambarini</Link></span>
-//       </div>
-//       <div className="navbar__left"><Link href="/home">alexandre</Link></div>
-//       <div className="navbar__center">
-//         <Link href="/projects" className="navbar__link link_projects">projects</Link>
-//         <Link href="/mylife" className="navbar__link link_mylife">my life</Link>
-//         <Link href="/contact" className="navbar__link  link_contact">contact</Link>
-//       </div>
-//       <div className="navbar__right"><Link href="/home">gambarini</Link></div>
-//     </nav>
-//   );
-// }
+export default function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="navbar__mobile-name">
+        <span><Link href="/home">alexandre</Link></span>
+        <span><Link href="/home">gambarini</Link></span>
+      </div>
+      <div className="navbar__left"><Link href="/home">alexandre</Link></div>
+      <div className="navbar__center">
+        <Link href="/projects" className="navbar__link link_projects">projects</Link>
+        <Link href="/mylife" className="navbar__link link_mylife">my life</Link>
+        <Link href="/contact" className="navbar__link  link_contact">contact</Link>
+      </div>
+      <div className="navbar__right"><Link href="/home">gambarini</Link></div>
+    </nav>
+  );
+}
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,11 +1,21 @@
-import './globals.css';
+// \src\app\layout.js
+import "./globals.css";
+import Navbar from './components/navbar';
+// import Cursor from '../utils/cursor'; // créer ce fichier ou mettre Cursor ici même
+
+export const metadata = {
+  title: "Alexandre Gambarini",
+  description: "Wanna work together ? :)",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="fr">
       <body>
+        <Navbar />
         {children}
-        </body>
+        {/* <Cursor /> */}
+      </body>
     </html>
   );
 }
